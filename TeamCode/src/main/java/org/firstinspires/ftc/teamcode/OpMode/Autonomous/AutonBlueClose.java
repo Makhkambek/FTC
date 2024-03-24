069aa8e0fa45cc1293bc5f94f7997622348a3323
Makhkambek <113063934+Makhkambek@users.noreply.github.com>
2024-03-25 00:26:59 +0500

    Update AutonBlueClose.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Autonomous/AutonBlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Autonomous/AutonBlueClose.java
@@ -1,60 +1,74 @@
 package org.firstinspires.ftc.teamcode.OpMode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import org.firstinspires.ftc.teamcode.OpMode.Autonomous.AutonBase;
-import org.firstinspires.ftc.teamcode.OpMode.Autonomous.SampleVisionOpMode;
-import org.firstinspires.ftc.teamcode.OpMode.Autonomous.CSVisionProcessor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import org.firstinspires.ftc.teamcode.OpMode.hardware.IntakeSystem;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.OpMode.hardware.ArmSystem;
+import org.firstinspires.ftc.teamcode.OpMode.hardware.IntakeSystem;
+import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.teamcode.OpMode.hardware.RobotHardwareMap;
 
+@Autonomous
+public class BlueClose extends AutonBase {
 
-@Autonomous(name = "Auton Blue Close To Backstage", group = "Autons")
-public class AutonBlueClose extends AutonBase {
-
-    private int parkingPosition = -1;
-    private int     lastParkingPosition = -1;
+    RobotHardwareMap theHardwareMap;
     IntakeSystem intakeSystem = new IntakeSystem(theHardwareMap, this); // Создание экземпляра класса IntakeSystem
     ArmSystem armSystem = new ArmSystem(theHardwareMap, this); // Создание переменной класса ArmSystem
+    private CSVisionProcessor visionProcessor;
+    private VisionPortal visionPortal;
 
     @Override
     public void runOpMode() {
         theHardwareMap = new RobotHardwareMap(hardwareMap, this );
         super.initialize();
 
-        boolean left = false;
-        boolean middle = false;
-        boolean right = true;
+        visionProcessor = new CSVisionProcessor(90,0,150,350,100,550,150);
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), visionProcessor);
 
-//        initialize();
+        CSVisionProcessor.StartingPosition startingPos = CSVisionProcessor.StartingPosition.NONE;
 
-        while (opModeInInit()) {
-//            //check that the camera is open and working
-//            armSystem.checkSensors(); // проверка состояния сенсоров
+//        waitForStart();
 
-
-            if (lastParkingPosition != parkingPosition) {
-                if (parkingPosition == 1) {
-                    telemetry.addData("Parking position", 1);
-                } else if (parkingPosition == 2) {
-                    telemetry.addData("Parking position", 2);
-                } else if (parkingPosition == 3) {
-                    telemetry.addData("Parking position", 3);
-                } else if (parkingPosition == 0) {
-                    telemetry.addData("Parking position", 0);
-                }
-                lastParkingPosition = parkingPosition;
-            }
+        while(!this.isStarted() && !this.isStopRequested()) {
+            startingPos = visionProcessor.getStartingPosition();
+            telemetry.addData("Identified", visionProcessor.getStartingPosition());
+            telemetry.update();
         }
 
-            waitForStart();
-            armSystem.checkSensors(); // проверка состояния сенсоров
-            //Set the arm position up to not drag
-            intakeSystem.stopIntake(); //интейк на 0 позиции
+        visionPortal.stopStreaming();
 
-            //Left
-            if (parkingPosition == 1) {
+        if (opModeIsActive()) {
+            //RIGHT side
+            if(startingPos == CSVisionProcessor.StartingPosition.RIGHT) {
+                if (startingPos == CSVisionProcessor.StartingPosition.RIGHT) {
+                    imuDrive(.4, 29.5, 0);
+                    imuTurn(.3, 90);
+                    imuDrive(.25, 7, 0);
+                    imuDrive(.15, -3, 0);
+
+                    //move intake down to deliver
+                    intakeSystem.startIntake();
+                    sleep(1000);
+                    intakeSystem.stopIntake();
+
+                    //drive towards pixel
+                    imuDrive(.2, -20, 0);
+                    encoderStrafe(0.5, -6.5, 5);
+                    imuDrive(.15, -14.5, 0);
+
+                    //deposit back pixel
+                    armSystem.startArm();
+                    sleep(1000);
+                    armSystem.stopArm();
+
+                    //slide out of the way
+                    encoderStrafe(0.5, 35, 5);
+                }
+            }
+        }
+        //LEFT side
+        else if(startingPos == CSVisionProcessor.StartingPosition.LEFT) {
+            if (startingPos == CSVisionProcessor.StartingPosition.LEFT) {
                 imuDrive(.4, 5, 0);
                 encoderStrafe(0.25, -11.75, 5);
                 imuDrive(0.25, 25, 0);
@@ -79,8 +93,10 @@ public class AutonBlueClose extends AutonBase {
                 encoderStrafe(0.5, 15, 5);
                 sleep(250);
             }
-            //Middle
-            else if (parkingPosition == 2) {
+        }
+        //CENTER side
+        else if(startingPos == CSVisionProcessor.StartingPosition.CENTER) {
+            if(startingPos == CSVisionProcessor.StartingPosition.CENTER) {
                 imuDrive(.3, 36, 0);
                 sleep(750);
                 imuDrive(.15, -5, 0);
@@ -103,39 +119,16 @@ public class AutonBlueClose extends AutonBase {
 
                 encoderStrafe(0.5, 30, 5);
             }
-            //Right
-            else if (parkingPosition == 3) {
-                imuDrive(.4, 29.5, 0);
-                imuTurn(.3, 90);
-                imuDrive(.25, 7, 0);
-                imuDrive(.15, -3, 0);
 
-                //move intake down to deliver
-                intakeSystem.startIntake();
-                sleep(1000);
-                intakeSystem.stopIntake();
-
-                //drive towards pixel
-                imuDrive(.2, -20, 0);
-                encoderStrafe(0.5, -6.5, 5);
-                imuDrive(.15, -14.5, 0);
-
-                //deposit back pixel
-                armSystem.startArm();
-                sleep(1000);
-                armSystem.stopArm();
-
-                //slide out of the way
-                encoderStrafe(0.5, 35, 5);
-            }
-            //Error unable to find target so slide to backdrop
-            else {
-                imuDrive(.5, 5, 0);
-                encoderStrafe(0.5, -47, 5);
-                telemetry.addData("Park Position Unknown", parkingPosition);
-            }
-
-            //set arm down in the end
-//            armMotor.moveArmEncoded(ArmPositions.FRONT_ARC_MIN);
         }
+        //NONE
+        else {
+            imuDrive(.5, 5, 0);
+            encoderStrafe(0.5, -47, 5);
+            telemetry.addData("Park Position Unknown", startingPos);
+        }
+
     }
+
+
+}
