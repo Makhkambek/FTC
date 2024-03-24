8dcfa8a43b5e81b3c764fca41267b68610444721
Makhkambek <okeydokeykokki@gmail.com>
2024-03-24 23:47:59 +0500

    Update ArmSystem.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Drive/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Drive/Drive.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.OpMode.Drive;
 
-import static java.lang.Thread.sleep;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
